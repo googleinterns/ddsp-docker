11282bcd242c0055e65bdb90953d1510074ceb67
ana-simionescu <anasimionescu@google.com>
2020-08-25 07:15:06 +0000

    Added more input prompts and default settings

--- user_interaction.py
+++ user_interaction.py
@@ -1,110 +1,108 @@
 #!/usr/bin/env python
 import os
 
-# Determining the way the library will be used
-modeOptions = {1 : "train", 2 : "eval", 3 : "sample"}
-
-print("You want to train, evaluate or sample a model?\n\
-[1] train\n[2] evaluate\n[3] sample")
-while True:
-    mode = input()
-    if mode not in modeOptions:
-        print("Insert only 1, 2 or 3")
-        continue
-    else:
-        mode = modeOptions[mode]
-        break
-
-print("You chose to " + mode + " the model.\n")
-
-# Determining if the image will run locally or remotely
-placeOptions = {1 : "locally", 2 : "on the Google Cloud AI Platform"}
-print("You want to " + mode + " locally or on the Google Cloud AI Platform?\n\
-[1] locally\n[2] Google Cloud AI Platform")
-while True:
-    place = input()
-    if place not in [1, 2]:
-        print("Insert only 1 or 2")
-        continue
-    else:
-        break
-
-print("You chose to " + mode + " " + placeOptions[place] + "\n")
-
-# Determining path for data retrieval
-dataPath = raw_input("Please insert a path for data retrieving. This can be a local directory or a GCS Bucket:\n")
+# # Determining path for data retrieval
+data_path = raw_input("Insert a path for data retrieving. \
+This can be a local directory or a GCS Bucket:\n")
 
 # Determining path for storing model, snapshots and summaries
-storingPath = raw_input("\nPlease insert a path for storing the model, snapshots and summaries.\
- This can be a local directory or a GCS Bucket:\n")
+storing_path = raw_input("\nInsert a path for saving the model, \
+snapshots and summaries. This can be a local directory or a GCS Bucket:\n")
 
-# Determining path for recovering a snapshot
-recoveryPath = raw_input("\nPlease insert path for recovering a snapshot or a completed model.\
- This can be a local directory or a GCS Bucket:\n")
+recovery_path = raw_input("\nInsert a path from which checkpoints will\
+ be restored before training. Skip for using the same path as for saving:\n")
+if recovery_path == "":
+    recovery_path = storing_path
 
-imageURI = raw_input("\nPlease insert a IMAGE URI:")
+gin_path = raw_input("\nInsert an additional gin file search path or skip\
+for only using the default paths:\n")
 
-configPath = raw_input("\nPlease insert the path to a configuration file:")
+image_URI = raw_input("\nPlease insert a IMAGE URI.\
+ The template is: gcr.io/<GCP_PROJECT_ID>/<name>:<tag> :")
+
+config_path = raw_input("\nPlease insert the path to a configuration file\
+ or skip for the default one:")
+if config_path == "":
+    config_path = "./utils/hypertune_configurations/config_hypertune.yaml"
 
 os.system("export PATH=/usr/local/google/home/$USER/.local/bin:$PATH")
 
-if place == 1:
-    #Building the image
-    buildCommand = "docker build -f local_training.Dockerfile -t " + imageURI + " ./" + " --build-arg data_path=" + dataPath
-    os.system(buildCommand)
+#Building the image
+build_command = "docker build -f Dockerfile -t " + image_URI + " ./"
+os.system(build_command)
+print("Docker image built")
 
-    print("Docker image built")
+# Push the image on Google Cloud Registry
+pushing_image = "docker push " + image_URI
+os.system(pushing_image)
+print("Imaged pushed to Google Cloud Registry")
 
-    # Run the image locally
-    runCommand = "docker run " + imageURI + " --mode=" + mode + " --save_dir=" + storingPath\
-    + " --alsologtostderr"\
-    + " --gin_file=models/solo_instrument.gin"\
-    + " --gin_file=datasets/tfrecord.gin"\
-    + " --gin_param=batch_size=16"\
-    + " --gin_param=train_util.train.num_steps=1"\
-    + " --gin_param=train_util.train.steps_per_save=1"\
-    + " --gin_param=trainers.Trainer.checkpoints_to_keep=1"
-    print("Running image...")
-    os.system(runCommand)
+print("Please insert a job name:")
+job_name = raw_input()
 
-    # Enabling Tensorboard
-    print("Enabling Tensorboard")
-    os.system("gcloud auth login")
-    tensorboardCommand = "tensorboard --logdir=" + storingPath + " --port=8080"
-    os.system(tensorboardCommand)
-elif place == 2:
-    #Building the image
-    buildCommand = "docker build -f ai_platform_training.Dockerfile -t " + imageURI + " ./"
-    os.system(buildCommand)
-    print("Docker image built")
+region = raw_input("\nPlease insert the region you want to train \
+your model in or skip for the default value (europe-west4):")
+if region == "":
+    region = "europe-west4"
 
-    # Push the image on Google Cloud Registry
-    pushing_image = "docker push " + imageURI
-    os.system(pushing_image)
-    print("Imaged pushed to Google Cloud Registry")
+batch_size = raw_input("\nPlease insert batch size\
+ or skip for the default value (128):")
+if batch_size == "":
+    batch_size = "128"
 
-    print("Please insert a job name:")
-    jobName = raw_input()
+no_of_steps = raw_input("\nPlease insert the number of steps for training\
+ or skip for the default value (10000):")
+if no_of_steps == "":
+    no_of_steps = "10000"
 
-    # Submit the job on AI Platform
-    print("Submitting the job on AI Platform")
-    submitting_job = "gcloud beta ai-platform jobs submit training " + jobName\
-    + " --region europe-west4 --master-image-uri " + imageURI + " --config " + configPath + " -- --mode=" + mode\
-    + " --save_dir=" + storingPath\
-    + " --alsologtostderr"\
-    + " --gin_file=models/solo_instrument.gin"\
-    + " --gin_file=datasets/tfrecord.gin"\
-    + " --gin_param=\"TFRecordProvider.file_pattern='" + dataPath + "/train.tfrecord*'\""\
-    + " --gin_param=magenta_ddsp_internals.trainers.Trainer.checkpoints_to_keep=10"\
-    + " --gin_param=early_stop_loss_value=5"\
-    + " --gin_param=magenta_ddsp_internals.train_util.train.batch_size=64"\
-    + " --gin_param=magenta_ddsp_internals.train_util.train.num_steps=10000"\
-    + " --gin_param=magenta_ddsp_internals.train_util.train.steps_per_summary=300"\
-    + " --gin_param=magenta_ddsp_internals.train_util.train.steps_per_save=300"
-    os.system(submitting_job)
+steps_per_save = raw_input("\nPlease insert the number of steps per save\
+ or skip for the default value (300):")
+if steps_per_save == "":
+    steps_per_save = "300"
 
-    # Enabling Tensorboard
-    print("Enabling Tensorboard")
-    os.system("gcloud auth login")
-    tensorboardCommand = "tensorboard --logdir=" + storingPath + " --port=8082"
-    os.system(tensorboardCommand)
+steps_per_summary = raw_input("\nPlease insert the number of steps per summary\
+ or skip for the default value (300):")
+if steps_per_summary == "":
+    steps_per_summary = "300"
+
+no_of_checkpoints = raw_input("\nPlease insert the number of checkpoints to \
+keep or skip for the default value (10):")
+if no_of_checkpoints == "":
+    no_of_steps = "10"
+
+early_stop_loss_value = raw_input("\nPlease insert the early stop loss value\
+ or skip for the default value (5):")
+if early_stop_loss_value == "":
+    early_stop_loss_value = "5"
+
+learning_rate = raw_input("\nPlease insert the learning rate\
+ or skip for the default value (0.001):")
+if learning_rate == "":
+    learning_rate = "0.001" 
+
+# Submit the job on AI Platform
+print("Submitting the job on AI Platform")
+submitting_job = "gcloud beta ai-platform jobs submit training " + job_name\
++ " --region " + region + " --master-image-uri " + image_URI + " --config " + config_path + " -- --mode=train"\
++ " --save_dir=" + storing_path\
++ " --restore_dir=" + recovery_path\
++ " --gin_search_path=" + gin_path\
++ " --alsologtostderr"\
++ " --hp_tuning=True"\
++ " --gin_file=models/solo_instrument.gin"\
++ " --gin_file=datasets/tfrecord.gin"\
++ " --gin_param=\"TFRecordProvider.file_pattern='" + data_path\
++ "/train.tfrecord*'\""\
++ " --gin_param=checkpoints_to_keep=" + no_of_checkpoints\
++ " --gin_param=early_stop_loss_value=" + early_stop_loss_value\
++ " --gin_param=magenta_ddsp_internals.train_util.train.batch_size=" + batch_size\
++ " --gin_param=magenta_ddsp_internals.train_util.train.num_steps=" + no_of_steps\
++ " --gin_param=magenta_ddsp_internals.train_util.train.steps_per_summary=" + steps_per_summary\
++ " --gin_param=magenta_ddsp_internals.train_util.train.steps_per_save=" + steps_per_save
+os.system(submitting_job)
+
+# Enabling Tensorboard
+print("Enabling Tensorboard")
+os.system("gcloud auth login")
+tensorboard_command = "tensorboard --logdir=" + storing_path + " --port=8082"
+os.system(tensorboard_command)
