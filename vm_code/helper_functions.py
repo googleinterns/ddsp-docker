d52383ec009766ddc68d929b6b55c10df808d8d1
anasimionescu <anasimionescu@google.com>
2020-09-09 19:31:54 +0300

    Added file uploading

---
+++ vm_code/helper_functions.py
@@ -0,0 +1,35 @@
+import os
+from google.cloud import storage
+from os import listdir
+
+def create_bucket():
+    """Creates a new bucket."""
+    bucket_name = "ddsp-training-web"
+    storage_client = storage.Client()   
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        print("Bucket {} exists".format(bucket.name))
+    except:
+        bucket = storage_client.create_bucket(bucket_name)
+        print("Bucket {} created".format(bucket.name))
+
+
+def upload_blob(uploads_dir):
+    """Uploads a file to the bucket."""
+    bucket_name = "ddsp-training-web"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    
+    for filename in listdir(uploads_dir):
+        source_file_name = os.path.join(uploads_dir, filename)
+        destination_blob_name = filename
+        
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(source_file_name)
+
+        print(
+            "File {} uploaded to {}.".format(
+                source_file_name, destination_blob_name
+            )
+        )
--- vm_code/main.py
+++ vm_code/main.py
@@ -1,14 +1,36 @@
-from flask import Flask, send_from_directory
+import os
+import helper_functions
+from werkzeug.utils import secure_filename
+from flask import Flask, send_from_directory, request, redirect, url_for, abort
+
 app = Flask(
     __name__,
     static_url_path='', 
     static_folder='../web_interface')
+app.config['UPLOAD_EXTENSIONS'] = ['.wav', '.mp3']
+app.config['UPLOAD_PATH'] = 'uploads'
 
+# Create a directory in a known location to save files to.
+uploads_dir = os.path.join(app.instance_path, app.config['UPLOAD_PATH'])
+os.makedirs(uploads_dir, exist_ok=True)
 
 @app.route('/')
 def main():
     return send_from_directory(app.static_folder, 'index_vm.html')
 
+@app.route('/', methods=['POST'])
+def upload_files():
+    for uploaded_file in request.files.getlist('file'):
+        filename = secure_filename(uploaded_file.filename)
+        if filename != '':
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            uploaded_file.save(os.path.join(uploads_dir, filename))
+    helper_functions.create_bucket()
+    helper_functions.upload_blob(uploads_dir)
+    return redirect(url_for('main'))
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+    
