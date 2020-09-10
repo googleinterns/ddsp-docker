fb9865924e953583645c41d5c347cd68ad84dd1f
anasimionescu <anasimionescu@google.com>
2020-09-10 17:12:14 +0300

    Run as sudo

--- vm_code/helper_functions.py
+++ vm_code/helper_functions.py
@@ -5,7 +5,7 @@ from os import listdir
 def create_bucket():
     """Creates a new bucket.""" 
     try:
-        os.system("gsutil mb gs://ddsp-train-web")
+        os.system("sudo gsutil mb gs://ddsp-train-web")
     except:
         print("Bucket ddsp-train-web exists")
     else:
@@ -14,7 +14,7 @@ def create_bucket():
 
 def upload_blob(uploads_dir):
     """Uploads files to the bucket."""
-    upload_command = "gsutil -m cp -r " + uploads_dir + " gs://ddsp-train-web" 
+    upload_command = "sudo gsutil -m cp -r " + uploads_dir + " gs://ddsp-train-web" 
     os.system(upload_command)
 
     print("Files uploaded to gs://ddsp-train-web.")
