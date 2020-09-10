b46ad77c8f624140751791de61035e8132dabca8
Weronika Ormaniec <werror@google.com>
2020-09-10 07:34:26 +0000

    Add data preprocessing

--- vm_code/main.py
+++ vm_code/main.py
@@ -4,6 +4,16 @@ app = Flask(
     static_url_path='', 
     static_folder='../web_interface')
 
+@app.route("/preprocess/", methods=['POST'])
+def preprocess():
+    preprocess_command = (
+        ["ddsp_prepare_tfrecord",
+         "--num_shards=10",
+         "--alsologtostderr",
+         "--input_audio_filepatterns=/usr/local/google/home/werror/Music/music/*wav",
+         "--output_tfrecord_path=/usr/local/google/home/werror/ddsp_dist/preprocessed"])
+    subprocess.run(args=ddsp_run_command, check=True)
+    #return send_from_directory(app.static_folder, 'index_vm.html')
 
 @app.route('/')
 def main():
