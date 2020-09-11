e926080a4465b6fb087019d9f3d0938230f8a950
Weronika Ormaniec <werror@google.com>
2020-09-11 08:26:06 +0000

    Add preprocessing

--- vm_code/helper_functions.py
+++ vm_code/helper_functions.py
@@ -1,6 +1,6 @@
+"""Helper function for GCP communication."""
+import datetime
 import os
-from google.cloud import storage
-from os import listdir
 
 def create_bucket():
     """Creates a new bucket.""" 
@@ -19,3 +19,18 @@ def upload_blob(uploads_dir):
 
     print("Files uploaded to gs://ddsp-train-web.")
         
+
+def run_preprocessing(bucket_name, region):
+    """Runs preprocessing job on AI Platform"""
+    job_name = f"preprocessing_job_{int((datetime.datetime.now()-datetime.datetime(1970,1,1)).total_seconds())}"
+    config_file = os.path.join(os.getcwd(), "../magenta_docker/config_single_vm.yaml") 
+    job_submission_command = (
+        f"gcloud ai-platform jobs submit training {job_name} "
+        f" --region {region} "
+        f" --config {config_file} "
+        " --master-image-uri $PREPROCESSING_IMAGE_URI "
+        "-- "
+        f"--input_audio_filepatterns={os.path.join(bucket_name, 'audio/*')} "
+        f"--output_tfrecord_path={os.path.join(bucket_name, 'tf_record/train.tfrecord')}")
+
+    os.system(job_submission_command)
--- vm_code/main.py
+++ vm_code/main.py
@@ -9,6 +9,7 @@ app = Flask(
     static_folder='../web_interface')
 app.config['UPLOAD_EXTENSIONS'] = ['.wav', '.mp3']
 app.config['UPLOAD_PATH'] = 'uploads'
+app.config['REGION'] = 'europe-west4'
 
 # Create a directory in a known location to save files to.
 uploads_dir = os.path.join(app.instance_path, app.config['UPLOAD_PATH'])
@@ -29,6 +30,10 @@ def upload_files():
             uploaded_file.save(os.path.join(uploads_dir, filename))
     helper_functions.create_bucket()
     helper_functions.upload_blob(uploads_dir)
+    return redirect(url_for('main'))
+
+@app.route('/preprocess', methods=['POST'])
+    helper_functions.run_preprocessing(app.config['BUCKET_NAME'], app.config['REGION'])
     return redirect(url_for('main'))
 
 if __name__ == '__main__':
