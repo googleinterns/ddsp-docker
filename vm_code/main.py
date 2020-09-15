1b5f14daccfdc0ed2780d09d96b45aca804f4859
anasimionescu <anasimionescu@google.com>
2020-09-15 20:35:59 +0300

    Added TensorBoard

--- vm_code/main.py
+++ vm_code/main.py
@@ -1,6 +1,7 @@
 """Main app script."""
 from datetime import datetime
 import os
+import subprocess
 
 from flask import abort, Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
@@ -18,6 +19,7 @@ app.config['REGION'] = 'europe-west4'
 app.config['BUCKET_NAME'] = (
     'gs://ddsp-train-' +
     str(int((datetime.now()-datetime(1970, 1, 1)).total_seconds())))
+app.config['TENSORBOARD_ID'] = ''
 
 # Create a directory in a known location to save files to.
 uploads_dir = os.path.join(app.instance_path, app.config['UPLOAD_PATH'])
@@ -43,15 +45,6 @@ def upload_files():
       app.config['REGION'])
   helper_functions.upload_blob(app.config['BUCKET_NAME'], uploads_dir)
   return render_template('index_vm.html')
-
-@app.route('/delete_bucket', methods=['POST'])
-def delete_bucket():
-  status = helper_functions.delete_bucket(app.config['BUCKET_NAME'])
-  if status == 'ERROR':
-    message = 'There was a problem deleting bucket :/'
-  else:
-    message = 'Bucket deleted successfully!'
-  return render_template('index_vm.html', message=message)
 
 @app.route('/preprocess', methods=['POST'])
 def preprocess():
@@ -112,7 +105,8 @@ def download_model():
       message = 'You haven\'t submitted training job yet!'
       return render_template('index_vm.html', message=message)
     elif status == 'SUCCEEDED':
-      helper_functions.get_model(app.config['BUCKET_NAME'], downloads_dir, app.instance_path)
+      helper_functions.get_model(app.config['BUCKET_NAME'],
+                                 downloads_dir, app.instance_path)
       download_zip = os.path.join(app.instance_path, 'model.zip')
       return send_file(download_zip, as_attachment=True)
     else:
@@ -122,6 +116,32 @@ def download_model():
     message = 'You haven\'t submitted training job yet!'
     return render_template('index_vm.html', message=message)
 
+@app.route('/tensorboard', methods=['POST'])
+def enable_tensorboard():
+  if 'JOB_NAME' in os.environ:
+    status = helper_functions.check_job_status(os.environ['JOB_NAME'])
+    if status == 'JOB_NOT_EXIST':
+      message = 'You haven\'t submitted training job yet!'
+      return render_template('index_vm.html', message=message)
+    elif status == 'RUNNING':
+      if app.config['TENSORBOARD_ID'] != '':
+        delete_experiment = ('tensorboard dev delete --experiment_id ' +
+                             app.config['TENSORBOARD_ID'])
+        os.system(delete_experiment)
+      else:
+        tensorboard_command = ('tensorboard dev upload --logdir ' +
+                               app.config['BUCKET_NAME'] + '/model ' +
+                               '--name DDSP_Training --one_shot')
+        link = subprocess.getoutput(tensorboard_command)
+        link = link.split()[-1]
+        app.config['TENSORBOARD_ID'] = link.split('/')[-2]
+        return render_template('index_vm.html', link=link)
+    else:
+      message = 'Training job status: ' + status
+      return render_template('index_vm.html', message=message)
+  else:
+    message = 'You haven\'t submitted training job yet!'
+    return render_template('index_vm.html', message=message)
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8080, debug=True)
